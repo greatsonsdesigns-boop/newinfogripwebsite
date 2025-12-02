a3e069dfac5442c5e5acf0d3489594e754e18d2f
Great Sons Designs <greatsonsdesigns@gmail.com>
2025-12-02 16:28:33 +0530

    Update thankyou.js

--- infogrip-payment-system/thankyou.js
+++ infogrip-payment-system/thankyou.js
@@ -1,6 +1,6 @@
 // Thank You Page Configuration
 const THANKYOU_CONFIG = {
-    API_BASE: 'https://script.google.com/macros/s/YOUR_APPS_SCRIPT_ID/exec'
+    API_BASE: 'https://script.google.com/macros/s/AKfycbx07QetFwOnkrHlNs2XWHKJUf-FVamBxzvr_ea75x1aJvV1A2wfsBIM3LkZZJfnalm5/exec'
 };
 
 // State
